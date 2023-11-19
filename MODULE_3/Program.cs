831a482c8eb4e02acf20f52e7406558a2107af36
Fedor <fedor777@mail.ru>
2023-11-19 16:52:09 +0300

    Работа с переменными - промежуточные успехи!

--- MODULE_3/Program.cs
+++ MODULE_3/Program.cs
@@ -5,11 +5,25 @@
         static void Main(string[] args)
         {
             string myName;
+            const string ConstName = "Fedor_Const";
             myName = "Fedor";
 
             Console.WriteLine(myName);
+            Console.WriteLine(ConstName);
+            Console.WriteLine("\tПривет, мир");
+            Console.WriteLine("\t\tМне 27 лет");
+            Console.WriteLine("My name is \nJane");
+            Console.WriteLine("Привет,\n мир");
+            Console.WriteLine("\u0040");
+            Console.WriteLine("\u0023");
+            Console.WriteLine(true);
+            Console.WriteLine(false);
+            Console.WriteLine(5);
+            Console.WriteLine(0x0A);
+            Console.WriteLine(0b11);
+            Console.WriteLine(5.5);
 
-            Console.ReadKey();
+            //          Console.ReadKey();
         }
     }
 }
